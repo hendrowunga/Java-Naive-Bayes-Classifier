2640a85e47bc27beb45d8d07e56438190bb35175
hendrowunga <hendrowunga073@gmail.com>
2025-04-10 14:18:08 +0700

    NaiveBayesClassifier

--- src/main/java/RunnableExample.java
+++ src/main/java/RunnableExample.java
@@ -1,75 +1,62 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-import de.daslaboratorium.machinelearning.classifier.bayes.BayesClassifier;
-import de.daslaboratorium.machinelearning.classifier.Classifier;
+import de.daslaboratorium.machinelearning.classifier.bayes.NaiveBayesClassifier;
+import de.daslaboratorium.machinelearning.classifier.data.DataHelper;
+import de.daslaboratorium.machinelearning.classifier.data.DataModel;
 
 public class RunnableExample {
 
     public static void main(String[] args) {
 
-        /*
-         * Create a new classifier instance. The context features are
-         * Strings and the context will be classified with a String according
-         * to the featureset of the context.
-         */
-        final Classifier<String, String> bayes =
-                new BayesClassifier<String, String>();
+//        String filePath = "src/dataset/comp.csv"; // Ganti dengan path yang sesuai
+         String filePath = "/home/endos/Documents/IntelijIdea/Java-Naive-Bayes-Classifier/ee/data.txt";
+        String fileType = "csv"; // Dapat berupa "csv" atau "txt"
+        String delimiter = ";"; // Hanya digunakan untuk file TXT
+        char separator = ';'; // Hanya digunakan untuk file CSV
 
-        /*
-         * The classifier can learn from classifications that are handed over
-         * to the learn methods. Imagin a tokenized text as follows. The tokens
-         * are the text's features. The category of the text will either be
-         * positive or negative.
-         */
-        final String[] positiveText = "I love sunny days".split("\\s");
-        bayes.learn("positive", Arrays.asList(positiveText));
+        List<DataModel> data = DataHelper.loadData(filePath, fileType, delimiter, separator);
 
-        final String[] negativeText = "I hate rain".split("\\s");
-        bayes.learn("negative", Arrays.asList(negativeText));
+        int numFolds = 5; // Jumlah lipatan dalam validasi silang
+        double totalAccuracy = 0.0;
 
-        /*
-         * Now that the classifier has "learned" two classifications, it will
-         * be able to classify similar sentences. The classify method returns
-         * a Classification Object, that contains the given featureset,
-         * classification probability and resulting category.
-         */
-        final String[] unknownText1 = "today is a sunny day".split("\\s");
-        final String[] unknownText2 = "there will be rain".split("\\s");
+        for (int fold = 0; fold < numFolds; fold++) {
+            // Bagi data menjadi set training dan pengujian untuk lipatan saat ini
+            List<DataModel> trainingData = new ArrayList<>();
+            List<DataModel> testingData = new ArrayList<>();
+            for (int i = 0; i < data.size(); i++) {
+                if (i % numFolds == fold) {
+                    testingData.add(data.get(i));
+                } else {
+                    trainingData.add(data.get(i));
+                }
+            }
 
-        System.out.println( // will output "positive"
-                bayes.classify(Arrays.asList(unknownText1)).getCategory());
-        System.out.println( // will output "negative"
-                bayes.classify(Arrays.asList(unknownText2)).getCategory());
+            // Latih classifier
+            NaiveBayesClassifier naiveBayes = new NaiveBayesClassifier();
+            naiveBayes.train(trainingData);
 
-        /*
-         * The BayesClassifier extends the abstract Classifier and provides
-         * detailed classification results that can be retrieved by calling
-         * the classifyDetailed Method.
-         *
-         * The classification with the highest probability is the resulting
-         * classification. The returned List will look like this.
-         * [
-         *   Classification [
-         *     category=negative,
-         *     probability=0.0078125,
-         *     featureset=[today, is, a, sunny, day]
-         *   ],
-         *   Classification [
-         *     category=positive,
-         *     probability=0.0234375,
-         *     featureset=[today, is, a, sunny, day]
-         *   ]
-         * ]
-         */
-        ((BayesClassifier<String, String>) bayes).classifyDetailed(
-                Arrays.asList(unknownText1));
+            // Evaluasi classifier
+            double accuracy = evaluate(naiveBayes, testingData);
+            System.out.println("Akurasi Lipatan " + (fold + 1) + ": " + accuracy);
+            totalAccuracy += accuracy;
+        }
 
-        /*
-         * Please note, that this particular classifier implementation will
-         * "forget" learned classifications after a few learning sessions. The
-         * number of learning sessions it will record can be set as follows:
-         */
-        bayes.setMemoryCapacity(500); // remember the last 500 learned classifications
+        double averageAccuracy = totalAccuracy / numFolds;
+        System.out.println("Akurasi Rata-Rata: " + averageAccuracy);
+    }
+
+    public static double evaluate(NaiveBayesClassifier naiveBayes, List<DataModel> testingData) {
+        int correctPredictions = 0;
+        for (DataModel row : testingData) {
+            String predictedCategory = naiveBayes.predict(row);
+            if (predictedCategory != null && predictedCategory.equals(row.getBuysComputer())) {
+                correctPredictions++;
+            }
+        }
+
+        return (double) correctPredictions / testingData.size();
     }
 
 }
